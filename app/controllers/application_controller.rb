2df218b8b58670b9d88157a68ce12437f4f34bdb
Mike Starr <imhs@protonmail.com>
2022-03-06 07:39:50 -0800

    commit tree 4894370319983f9d769f05abf5d7108c3fceb123

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < Sinatra::Base
 
   # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    bakery = Bakery.all
+    bakery.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_good_price = BakedGood.all.order(price: :desc)
+    baked_good_price.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    most_expensive = BakedGood.all.order(price: :desc).first
+    most_expensive.to_json
+  end
+
 
 end
